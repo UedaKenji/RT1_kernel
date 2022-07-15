00b1dd4736482fff003d5c6783111f40f508b75e
Kenji Ueda <ueda.kenji19@ae.k.u-tokyo.ac.jp>
2022-07-15 13:59:23 +0900

    add: tomography.oy 追加

--- rt1kernel/__init__.py
+++ rt1kernel/__init__.py
@@ -1,4 +1,5 @@
 import sys,os 
 from .rt1kernel import *
+from . import tomography
 from . import plot_utils
 #sys.path.insert(0, os.path.abspath(os.curdir)+'\\rt1raytrace')
---
+++ rt1kernel/tomography.py
@@ -0,0 +1,24 @@
+from pkgutil import extend_path
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy import FPE_DIVIDEBYZERO, array, linalg, ndarray
+import rt1plotpy
+from typing import Optional, Union,Tuple,Callable,List
+import time 
+import math
+from tqdm import tqdm
+import scipy.linalg as linalg
+from numba import jit
+import warnings
+from dataclasses import dataclass
+import itertools
+import scipy.sparse as sparse
+import pandas as pd
+import os,sys
+from .plot_utils import *  
+
+import rt1kernel
+
+sys.path.insert(0,os.pardir)
+import rt1raytrace
+
